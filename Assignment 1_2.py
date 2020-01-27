b6ac1eb1a09884df4e0a922eedc4e2dbd286c26e
Suryakant-Kamble <59169964+Suryakant-Kamble@users.noreply.github.com>
2020-01-27 12:52:23 +0530

    Check number Even Or odd
    Accept one parameter as the number and if the number is even then it  should display "Even Number" otherwise display "Odd Number" on console

---
+++ Assignment 1_2.py
@@ -0,0 +1,11 @@
+def ChkNum():
+    a = int(input("Enter the Number : "))
+    if a % 2 == 0:
+        print("Even Number")
+    else:
+        print("Old Number")
+
+
+if __name__ == '__main__':
+    ChkNum()
+
