885323f35c31681b27cc5bcf339f9f69b069a932
Suryakant-Kamble <59169964+Suryakant-Kamble@users.noreply.github.com>
2020-01-27 13:01:08 +0530

    One function named as Add()
    which accepts two numbers from the user and return addition of that two numbers

---
+++ Assignment1_3.py
@@ -0,0 +1,9 @@
+def Add():
+    a= int(input("Enter the 1st Number:  "))
+    b= int(input("Enter the 2nd Number:  "))
+    ret= a + b
+    print("Addition is = ",ret)
+
+
+if __name__ == '__main__':
+    Add()
